536693139a5b05b9b13bcb0f6a16c11e12923b20
hiro <hiro.net.study@gmail.com>
2023-09-08 16:40:45 +0900

    message

--- src/routes/web.php
+++ src/routes/web.php
@@ -7,4 +7,4 @@ Route::get('/find', [AuthorController::class, 'find']);
 Route::post('/find', [AuthorController::class, 'search']);
 Route::get('/author/{author}', [AuthorController::class, 'bind']);
 Route::get('/index', [AuthorController::class, 'index']);
-Route::post('/add', [AuthorController::class, 'add']);
+Route::get('/add', [AuthorController::class, 'add']);
